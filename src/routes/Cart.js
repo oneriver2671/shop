ecceed43d424b987a16b89a17ca8335c4937b160
oneriver jang <skijh5@gmail.com>
2022-09-15 20:49:32 +0900

    Feat: Redux 사용해보기

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,30 +1,36 @@
 import Table from 'react-bootstrap/Table';
+import { useSelector } from 'react-redux';
 
 function Cart(){
+
+  // useSelector: Redux store를 가져와줌
+  let test = useSelector((state) => { return state.stock });
+  let userStoreItemArr = useSelector((state) => { return state.userStoreArr });
+
   return(
     <div>
        <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
+          <th>상품 코드</th>
+          <th>상품명</th>
+          <th>갯수</th>
           <th>Username</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
+        { 
+          userStoreItemArr.map(function(item){
+            return (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.count}</td>
+                <td>oneriver</td>
+              </tr>
+            )
+          }) 
+        }
         <tr>
           <td>3</td>
           <td colSpan={2}>Larry the Bird</td>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+
+// useState() 역할과 비슷. state 하나를 slice라고 부름.
+let user = createSlice({
+  name: 'user',
+  initialState: 'jang'
+})
+
+let stock = createSlice({
+  name: 'stock',
+  initialState: [10, 11, 12]
+})
+
+// 과제: 장바구니 data 사용
+let _userStoreArr = [{id : 0, name : 'White and Black', count : 2}, {id : 2, name : 'Grey Yordan', count : 1}]; 
+let userStoreArr = createSlice({
+  name: 'userStoreArr',
+  initialState: _userStoreArr
+})
+
 
 export default configureStore({
   reducer: {
-    
+    user: user.reducer,
+    stock: stock.reducer,
+    userStoreArr: userStoreArr.reducer
   }
 })
